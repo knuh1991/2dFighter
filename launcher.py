3c7f3b6a30451f573059156b5de69ffeb67ca939
Khalid <56797035+knuh1991@users.noreply.github.com>
2019-11-13 12:26:37 +0000

    First Commit of the Launcher file. 800*600 black window is displayed

---
+++ launcher.py
@@ -0,0 +1,38 @@
+import sys
+import pygame
+
+class Launcher:
+    """ The Launcher class that will be the first view when the game is initiated."""
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800,600))
+        pygame.display.set_caption("Best Adevnture Game Ever")
+        self.bg_color =(230,230,230)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+
+    def _check_keyup_events(self,event):
+        pass
+    def _check_keydown_events(self,event):
+        pass
+
+
+if __name__ == '__main__':
+    launcher = Launcher()
+    launcher.run_game()
